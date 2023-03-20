f6d8576fb8f825a982a2b382a89db0998b0e18a6
bosesirsendu352 <117670366+bosesirsendu352@users.noreply.github.com>
2023-03-20 21:22:53 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
- Configure the AWS provider
+ #Configure the AWS provider
 provider "aws" {
   region = "us-east-1"
 }
+
 # Launch an EC2 instance from an AMI golden image
 resource "aws_instance" "web" {
-  ami           = "ami-0e0efb15f9d611a0d"
+  ami           = "ami-0a395f26e6c39056f"
   instance_type = "t2.micro"
 }
+
 resource "aws_security_group" "http_security_group" {
   name_prefix = var.security_group_name
   description = var.security_group_description
@@ -17,4 +19,5 @@ resource "aws_security_group" "http_security_group" {
     protocol    = "tcp"
     cidr_blocks = var.allowed_cidr_blocks
   }
+ 
 }
