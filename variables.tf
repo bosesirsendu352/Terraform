95a9d5ecf5763dd377900c98c11d2733ccf85e0e
bosesirsendu352 <117670366+bosesirsendu352@users.noreply.github.com>
2023-03-20 21:25:38 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,12 +13,5 @@ variable "security_group_description" {
 variable "allowed_cidr_blocks" {
   description = "List of CIDR blocks allowed to access the instance via HTTP"
   type        = list(string)
- type        = string
-  default     = "Security group that allows HTTP traffic"
-}
-
-variable "allowed_cidr_blocks" {
-  description = "List of CIDR blocks allowed to access the instance via HTTP"
-  type        = list(string)
   default     = ["0.0.0.0/0"]
 }
